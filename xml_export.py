afe59a3d5a537fe49b9dbe1013d7c738aabd0875
Vanell <geof.gaill@gmail.com>
2015-06-04 13:06:43 +0200

    add lib sorted

---
+++ xml_export.py
@@ -0,0 +1,15 @@
+import xml.etree.cElementTree as ET
+
+
+def xml_export(layers,name):
+  
+  PRODUCT = ET.Element("PRODUCT")
+  for layer in layers:
+  
+    for part in layers[layer]:
+      
+      POSI = ET.SubElement(PRODUCT, "POSI",x=str(part[0][1]),y=str(part[1][1]),z=str(part[2][1]),w=str(part[3][1]),p=str(part[4][1]), r=str(part[5][1]), config_data=str(part[6][1]))
+    
+  
+  tree = ET.ElementTree(PRODUCT)
+  tree.write(str(name))
