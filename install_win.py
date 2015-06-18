6895593f9000c7e45502e07a26813d198065573d
Vanell <geof.gaill@gmail.com>
2015-06-18 14:02:53 +0200

    change install file

--- install_win.py
+++ install_win.py
@@ -1,28 +1,29 @@
 import os
 import urllib.request, urllib.parse, urllib.error
  
-directory_py = 'c:\Python33\Lib\Fanuc_xml\\'
+directory = 'c:\Gen_Xml\\'
 file_name_py = 'tri_macro.py'
-file_name_vba = 'macro_catia.vba'
+file_name_vba = 'IHM.catvba'
 destination_py = directory_py+file_name_py
 destination_vba = directory_vba+file_name_vba
  
-url_python = "https://raw.githubusercontent.com/Vanell/TX_P15/master/tri_macro.py"
-url_vba = "https://raw.githubusercontent.com/Vanell/TX_P15/master/macro_xml_catia.vba"
+url_python = "https://github.com/Vanell/TX_P15/raw/master/tri_macro.py"
+url_vba = "https://github.com/Vanell/TX_P15/raw/master/IHM.catvba"
  
 print("Downloading tri macro python")
 urllib.request.urlretrieve(url_python, file_name_py)
 print("Downloading tri macro vba")
 urllib.request.urlretrieve(url_python, file_name_vba)
 
-print("Create folder for python marco")
-if not os.path.exists(directory_py):
-    os.makedirs(directory_py)
+print("Create folder for marco")
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 print("Mouve python marco")
 #Mouve python script
-os.rename(file_name_py, destination_py)
+os.rename(file_name_py, destination)
 print("Mouve vba marco")
 #Mouve catia macro
-os.rename(file_name_vba, destination_vba)
+os.rename(file_name_vba, destination)
 print('Install Done !')
+raw_input()
